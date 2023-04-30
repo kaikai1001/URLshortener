f71ddc2138d3fc1df146af084bb95064e90f1310
kaiaki1001 <cccjjmm09@gmail.com>
2023-05-01 03:56:12 +0800

    feat: add README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# 短網址產生器
+## Features
+* 輸入網址轉換成短網址
+* 在瀏覽器輸入轉換後的短網址可回到原本網址
+* 
+# How to use
+1. 先安裝好Node.js 和 npm
+2. 將所有檔案clone至本機
+3. 打開終端機cd至資料夾並輸入:
+```
+npm install
+```
+4. 安裝後,輸入:
+```
+npm run dev
+```
+5. 若出現以下訊息代表成功，進入瀏覽器輸入以下網址
+```
+APP is running on http://localhost:3000
+```
+6. 若要退出，在終端機按下 Ctrl + c
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 
-//process.env.MONGODB_URI
-mongoose.connect('mongodb+srv://alpha:camp@cluster0.rdlqvcs.mongodb.net/url-list?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
 
